e2e799e6b93e9aa82af3f70c81f7d40cd764a1ef
herdianto <herdi.16@gmail.com>
2017-10-19 17:51:46 +0700

    product management

--- api/routes/index.js
+++ api/routes/index.js
@@ -25,6 +25,7 @@ router.post('/api/admin/member_approval', user_service.set_member_status);
  
 //product service
 router.get('/api/product/search', product_service.search);
+router.post('/api/product/buy', product_service.buy);
 router.post('/api/admin/product/search', product_service.search_admin);
 router.post('/api/admin/product/insert', product_service.create);
 router.post('/api/admin/product/update', product_service.update);
--- api/routes/products_service.js
+++ api/routes/products_service.js
@@ -5,6 +5,7 @@ var query = require('../helper/db_connection');
 var config = require('../config/config.json');
 var mysql = require('mysql');
 var path = require('path');
+var waterfall = require('a-sync-waterfall');
 
 var product_service = {
   create: function(req, res) {
@@ -82,44 +83,63 @@ var product_service = {
     let obats = new Array();
     let images = new Array();
     let obat = new Object();
-    let params_select =['%'+req.query.keyword+'%', '%'+req.query.keyword+'%'];
-    let query_cmd_select = "SELECT * FROM product WHERE name LIKE ? OR description LIKE ? and status ='active';"
-    query(mysql.format(query_cmd_select, params_select)).
-    then(function(result){
-      for(let i=0; i<result.length; i++){
-        let params_select = [result[i].product_id];
-        let query_cmd_select = "SELECT product_id, url FROM attachment_url WHERE product_id = ?;"
-        console.log(mysql.format(query_cmd_select, params_select));
-        query(mysql.format(query_cmd_select, params_select)).then(function(images_data){
-          for(let j=0; j<images_data.length; j++){
-            images[j] = "/images/"+images_data[j].url;
-            console.log(images_data[j].url);
+    waterfall([
+      function getMasterProduct(callback){
+        let params_select =['%'+req.query.keyword+'%', '%'+req.query.keyword+'%'];
+        let query_cmd_select = "SELECT * FROM product WHERE (name LIKE ? OR description LIKE ?) AND status ='active';"
+        query(mysql.format(query_cmd_select, params_select)).
+          then(function(result){
+            for(let i=0; i<result.length; i++){
+              obat = {
+                product_id: result[i].product_id,
+                name: result[i].name,
+                description: result[i].description,
+                member_price: result[i].member_price,
+                non_member_price: result[i].non_member_price,
+                posted_date: result[i].posted_date,
+                posted_by: result[i].posted_by,
+                last_update_date: result[i].last_update_date,
+                last_update_by: result[i].last_update_by,
+                product_images:images 
+              };
+              obats[i]=obat;
+            }
+            callback(null, obats);
+          })
+      },
+      function getProductImages(obats, result){
+        var counter = 0;
+        for(let i=0; i<obats.length; i++){
+          let query_cmd_select = "SELECT product_id, url FROM attachment_url WHERE product_id = ?;"
+          let params_select = [obats[i].product_id];
+          product_service.get_single_images(params_select,function(x){
+            obats[i].product_images = x;
+            data();
+          });
+        }
+        function data(){
+          counter++;
+          if(counter == obats.length){
+            result(null, obats);
           }
-        })
-        obat = {
-          product_id: result[i].product_id,
-          name: result[i].name,
-          description: result[i].description,
-          member_price: result[i].member_price,
-          non_member_price: result[i].non_member_price,
-          posted_date: result[i].posted_date,
-          posted_by: result[i].posted_by,
-          last_update_date: result[i].last_update_date,
-          last_update_by: result[i].last_update_by,
-          product_images:images 
-        };
-        obats[i]=obat;
+        }
       }
-      res.status(config.http_code.ok);
-      res.json(obats);
-    })
-    .catch(function(error){
-      console.log("error: "+error);
-      res.status(config.http_code.in_server_err);
-      res.json({
-          "status": config.http_code.in_server_err,
-          "message": "Internal Server Error"
-      });
+    ], 
+      function (err, result){
+        res.status(config.http_code.ok);
+        res.json(result);
+      }
+    );
+  },
+  get_single_images: function(prd_id, result){
+    let params_select = [prd_id];
+    let query_cmd_select = "SELECT product_id, url FROM attachment_url WHERE product_id = ?;"
+    var images = [];
+    query(mysql.format(query_cmd_select, params_select)).then(function(images_data){
+      for(let j=0; j<images_data.length; j++){
+        images[j] = "/images/"+images_data[j].url;
+      }
+      result(images);
     });
   },
   search_admin: function(req, res){
@@ -162,15 +182,49 @@ var product_service = {
   },
   update: function(req, res){
     let params = req.body;
+    let counter = 0;
     for(let i=0; i<req.body.product.length; i++){
-      let product = req.body.product[i];
-      let admin_name = jwt.decode(params.token, config.jwt_signature).user;
-      let current_time = new Date();;
-      let params_update =[product.name, product.description, product.member_price, product.non_member_price, current_time, admin_name, product.status, product.product_id];
-      let query_cmd_update = "UPDATE product SET name=?, description=?, member_price=?, non_member_price=?, last_update_date=?, last_update_by=?, status=? WHERE product_id= ?;"
-      console.log(mysql.format(query_cmd_update, params_update));
+      product_service.update_single_product(req.body.product[i], req.body.token, function(result){
+        report();
+        if(result!="success"){
+          i=req.body.product.length
+          res.status(config.http_code.in_server_err);
+          res.json({
+              "status": config.http_code.in_server_err,
+              "message": "Internal Server Error"
+          });
+        }
+      });
     }
-    res.json("asd");
+    function report(){
+      counter++;
+      if(req.body.product.length == counter){
+        res.status(config.http_code.ok);
+        res.json({
+          "status": 200,
+          "message": "Successfully Updated"
+      });
+      }
+    }
+  },
+  update_single_product: function(product, token, callback){
+    let admin_name = jwt.decode(token, config.jwt_signature).user;
+    let current_time = new Date();
+    let params_update =[product.name, product.description, product.member_price, product.non_member_price, current_time, admin_name, product.status, product.product_id];
+    let query_cmd_update = "UPDATE product SET name=?, description=?, member_price=?, non_member_price=?, last_update_date=?, last_update_by=?, status=? WHERE product_id= ?;"
+    //console.log(product);
+    //console.log(mysql.format(query_cmd_update, params_update));
+    query(mysql.format(query_cmd_update, params_update))
+    .then(function(result){
+      callback("success");
+    })
+    .catch(function(error){
+      console.log("error: "+error);
+      callback("error");
+    });
+  },
+  buy: function(req, res){
+    res.json("buy");
   }
 };
  
